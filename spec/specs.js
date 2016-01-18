27fbc030ffbd02c33c5103f7fa2d44bebf840e51
Michael Gallegos <mklgallegos@gmail.com>
2016-01-18 11:16:36 -0800

    add non-leap year functionality

---
+++ spec/specs.js
@@ -0,0 +1,11 @@
+
+// first argument 'leapYear' is the function we intend to write
+// second argument is the it function. The it function takes a
+// string as its first argument, too. The second argument is 
+// the function which contains the assertion. 
+
+describe ('leapYear', function() {
+	it("is false for a year that is not divisible by 4, 100, or 400", function() {
+		expect(leapYear(1993)).to.equal(false);
+	});
+});
