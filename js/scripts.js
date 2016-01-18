019c696688fe5221f2dafa89e3b3f706feb6b7ed
Michael Gallegos <mklgallegos@gmail.com>
2016-01-18 11:39:32 -0800

    refactor leapYear to a single return expression

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 var leapYear = function(year) {
 
+	return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0));
 
-	if ((year % 4 === 0) && (year % 100 !== 0)) {
-		return true;
-	} else {
-		return false;
-	}
+	// if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+	// 	return true;
+	// } else {
+	// 	return false;
+	// }
 
 	// if (year % 100 === 0) {
 	// 	return false;
