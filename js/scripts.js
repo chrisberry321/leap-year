59d0b993411fb9c84e79a3b677af57147e5c7323
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-19 09:15:15 -0800

    add specs for Nan, negative numbers and words

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@
 
 var leapYear = function(year) {
 
-	return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0));
+	// return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0));
 
-	// if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-	// 	return true;
-	// } else {
-	// 	return false;
-	// }
+	if ( year > 0 && ((year % 4 === 0) && (year % 100 !== 0)|| (year % 400 === 0))) {
+		return true;
+	} else if (year === NaN) {
+		return false;
+	} else {
+		return false;
+	}
 
 	// if (year % 100 === 0) {
 	// 	return false;
-	// } else if 
+	// } else if
 	// 	(year % 4 === 0) {
 	// 	return true;
 	// } else {
@@ -24,18 +26,24 @@ var leapYear = function(year) {
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
+     var year = parseInt($("input#year").val());
+     var result = leapYear(year);
 
     $(".year").text(year);
 
-    if (!result) {       // same as writing if (result === false)
+		if (year === NaN || year < 0 ) {
+			alert("OMG INVALID INPUT");
+			$(".not").text("not");
+		}
+    else if (!result) {       // same as writing if (result === false)
       $(".not").text("not");
+			$("#result").show();
     } else {
       $(".not").text("");
+			$("#result").show();
     }
 
-    $("#result").show();
+
     event.preventDefault();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 
 // first argument 'leapYear' is the function we intend to write
 // second argument is the it function. The it function takes a
-// string as its first argument, too. The second argument is 
-// the function which contains the assertion. 
+// string as its first argument, too. The second argument is
+// the function which contains the assertion.
 
 describe ('leapYear', function() {
 	it("is false for a year that is not divisible by 4, 100, or 400", function() {
@@ -19,5 +19,13 @@ describe ('leapYear', function() {
 
 	it ("is true for years divisible by 400", function() {
 		expect (leapYear(2000)).to.equal(true);
+	});
+
+	it ("is false for blank input", function() {
+		expect (leapYear(NaN)).to.equal(false);
+	});
+
+	it ("is false for negavtive integers", function() {
+		expect (leapYear(-400)).to.equal(false);
 	});
 });
